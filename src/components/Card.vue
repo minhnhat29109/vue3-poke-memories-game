db7eaecb879351682e19e2bc1cbcb99ae42d316c
minh nhat <nhatdm1@gmail.com>
2023-11-08 08:33:43 +0700

    08-11

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="card">
-    <div class="card__inner">
+  <div class="card" :class="{ disabled: isDisabled }">
+    <div
+      class="card__inner"
+      :class="{ 'is-flipped': isFlipped }"
+      @click="onToggleFlipCard"
+    >
       <div class="card__face card__face--front">
         <div class="card__content">Front</div>
       </div>
@@ -10,3 +14,57 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isFlipped: false,
+    };
+  },
+  methods: {
+    onToggleFlipCard() {
+      this.isFlipped = !this.isFlipped;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.card {
+  width: 100px;
+  height: 150px;
+  display: inline-block;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+}
+
+.card__inner {
+  width: 100%;
+  height: 100%;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+  cursor: pointer;
+  position: relative;
+}
+
+.card__inner.is-flipped {
+  transform: rotateY(-180deg);
+}
+
+.card__face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  overflow: hidden;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 0 3px 18px 3px rgba(231, 8, 8, 0.2);
+}
+
+.card__face--back {
+  background-color: var(--light);
+  transform: rotateY(-180deg);
+}
+</style>
